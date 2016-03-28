983607bbc7aeccd9ac20945e8df729f9562e4b18
André Maia <pitymaia@gmail.com>
2016-03-28 18:43:48 -0300

    CSS style

--- angularcrumbtrail.js
+++ angularcrumbtrail.js
@@ -4,9 +4,9 @@ app.directive('angularCrumbtrail', function () {
 	return {
 		scope: true,
 		restrict: 'AE',
-		template:   '<ol class="breadcrumb" style="margin-bottom: 5px;">' +
-					'<li data-ng-repeat="breadcrumb in breadcrumbs.get()" data-ng-class="{ active: $last }">' +
-						'<a href="#" data-ng-bind="breadcrumb"></a>' +
+		template:   '<ol class="breadcrumb">' +
+					'<li data-ng-repeat="breadcrumb in breadcrumbs.get()">' +
+						'<a href="" data-ng-bind="breadcrumb" data-ng-class="{ active: $last }"></a>' +
 					'</li>' +
 					'</ol>',
 		link: function ($scope, element, attrs) {
